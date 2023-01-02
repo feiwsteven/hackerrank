1516ccc10540bae9011b83fb7287b38fbf836d7d
fei Wang <wafei@wafei-00.local>
2023-01-02 09:37:38 -0500

    first commit

---
+++ simple_array_sum.py
@@ -0,0 +1,36 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    # Write your code here
+    s = 0
+    for x in ar:
+        s += x
+    return s
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.getcwd()+'/output.txt', 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    print(result)
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
