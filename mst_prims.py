1bdc0b039a07e2259d4cf9470822386a73e870ac
fei Wang <wafei@wafei-00.local>
2023-02-19 12:14:10 -0500

    add mst

---
+++ mst_prims.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    g = int(input())
+
+    for g_itr in range(g):
+        nms = input().split()
+
+        n = int(nms[0])
+
+        m = int(nms[1])
+
+        s = int(nms[2])
+
+        # Write Your Code Here
